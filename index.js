727207c73e5fd940ef4592f5976702ad1235cbe2
Samyak Maharjan <samyak.maharjan@cloudfactory.com>
2021-12-06 20:26:08 +0545

    use multiple services/containers

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//can use ip address instead of mongo while connecting, since ip may change whenever we run a container, it is not used
+//directly. Instead, service name is used
+mongoose
+  .connect("mongodb://root:samyak@mongo:27017/?authSource=admin")
+  .then(() => console.log("successfully coneected to DB"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("<h2>Alright! working</h2>");
