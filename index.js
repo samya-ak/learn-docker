d406b3bdf995c15bd4c9fa48a4e49e5376b8a405
Samyak Maharjan <samyakjyapu33127@gmail.com>
2023-01-04 22:22:44 +0545

    install packages

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const {
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const session = require("express-session");
+const redis = require("redis");
+const cors = require("cors");
+let RedisStore = require("connect-redis")(session);
+
 //can use ip address instead of mongo while connecting, since ip may change whenever we run a container, it is not used
 //directly. Instead, service name is used
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
+
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const connectWithRetry = () => {
   mongoose
-    // here mongo is the name of the service which is resolved to its respective ip address
     .connect(mongoURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -27,9 +40,32 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
-app.get("/", (req, res) => {
-  res.send("<h2>Alright! working</h2>");
+app.enable("trust proxy");
+app.use(cors({}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
+
+app.use(express.json());
+
+app.get("/api/v1", (req, res) => {
+  res.send("<h2>Hi  There</h2>");
+  console.log("it ran");
 });
+
+//localhost:3000/api/v1/post/
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listenning on port: ${port}`));
